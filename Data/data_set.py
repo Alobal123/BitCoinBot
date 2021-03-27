7b902029500a3a884917b25cec40ded8cf37abe9
Miroslav Krabec <miroslavkrabec@seznam.cz>
2021-03-27 10:01:45 +0100

    Add training and testing split and generation of episodes

--- Data/data_set.py
+++ Data/data_set.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import pandas as pd
 
 
@@ -9,11 +10,19 @@ class DataSet:
         self.data = pd.concat([pd.read_csv(os.path.join(path, f), usecols=[0, 1, 3]) for f in os.listdir(path)])
         self.data.sort_values(by=['Unix Timestamp'], inplace=True)
 
-    def get_data(self, range: int = 15):
-        data = self.data.iloc[::range, :]
+    def get_data(self, interval: int = 15):
+        data = self.data.iloc[::interval, :]
         data.reset_index(inplace=True, drop=True)
         return data
 
+    def get_training_split(self, interval: int, duration_of_episode):
+        data = self.get_data(interval=interval)
+        N = data.shape[0]
+        dfs = np.array_split(data, N//duration_of_episode * interval)
+        return dfs[0: len(dfs)//10*8], dfs[len(dfs)//10*8: ]
+
 
 if __name__ == '__main__':
-    DataSet('original')
+    dataset = DataSet('original')
+    train, test = dataset.get_training_split(interval=10, duration_of_episode=600)
+    print(len(train), len(test))
--- Data/test_data_set.py
+++ Data/test_data_set.py
@@ -8,6 +8,6 @@ class TestDataset:
     def test_data_set(self):
         dataset = DataSet('test_resources/test_1')
         expected = pd.read_csv('test_resources/expected.csv')
-        print(dataset.get_data(range=2))
-        print(expected)
-        assert_frame_equal(dataset.get_data(range=2), expected)
+        assert_frame_equal(dataset.get_data(interval=2), expected)
+
+        print(dataset.get_training_split(2,3))
